6cd62bfaf0b803f5db070e35f32af651f7aa5e45
Ju-HyeongWoo <gch04319@naver.com>
2022-03-08 19:09:28 +0900

    10430

---
+++ 기초-수학/4375.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+
+n = int(input())
+i = 0
+            # a = 10^0부터 10^i 까지의 합
+            # a 를 n으로 나누어서 나머지가 0이 아니면 i를 1씩 증가
+            # a 를 n으로 나누어서 나머지가 0이면 i를 출력
+
+    
