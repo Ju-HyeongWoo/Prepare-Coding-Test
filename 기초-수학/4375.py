49fb833dff01d8ced85b93546c253fc8a071484a
Ju-HyeongWoo <gch04319@naver.com>
2022-07-26 19:05:32 +0900

    edit 4375

--- 기초-수학/4375.py
+++ 기초-수학/4375.py
@@ -1,19 +1,9 @@
-from operator import truediv
-
 n = int(input())
-x = 1
-a = 1
-m = 1
-while m != 0:
-    for i in range(x):
-        a += 10 ** i
-        m = a % n
-        x += 1
-    print(x)
+
 
     
     
                      # a 를 n으로 나누어서 나머지가 0이 아니면 i를 1씩 증가
-                                # a 를 n으로 나누어서 나머지가 0이면 i를 출력
+                     #  # a 를 n으로 나누어서 나머지가 0이면 i를 출력
 
     
---
+++ 단계별/2480.py
@@ -0,0 +1,4 @@
+a, b, c = map(int,input().split())
+s = 0
+if a == b and b == c:
+    s = 10000 + a * 10
